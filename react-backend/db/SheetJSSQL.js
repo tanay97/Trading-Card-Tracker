0676a0e1727ddcb50695f9f4c7aa4699db8d80e7
stanley9966 <stanley9966@hotmail.com>
2018-09-12 02:43:55 -0400

    faster upload, needs cleanup and error checks

--- react-backend/db/SheetJSSQL.js
+++ react-backend/db/SheetJSSQL.js
@@ -1,81 +1,53 @@
 var X = require('xlsx');
+const pgp = require('pg-promise')({
+    capSQL: true, // if you want all generated SQL capitalized
+});
+const Column = pgp.helpers.Column;
 
-var _TYPES = {
-  "PGSQL":  { t:"text", n:"float8", d:"timestamp", b:"boolean" },
-  "MYSQL":  { t:"TEXT", n:"REAL",   d:"DATETIME",  b:"TINYINT" },
-  "SQLITE": { t:"TEXT", n:"REAL",   d:"TEXT",      b:"REAL"    }
-};
+var headers = [];
 
 module.exports = {
-	// takes excel sheet and parses into array of queries
-	//NOTE! PRIMARY KEY must be the first column
-	//NOTE! can't have same column name...
-	sheetToQueries: function(ws, sname, mode) {
-		var TYPES = _TYPES[mode || "SQLITE"];
-		console.log("inside sheetToQueries function");
+
+	getHeaders: function() {
+		return headers;
+	},
+
+	// takes excel sheet, and generates ColumnSet object
+	createColumnSet: function(ws, sname, mode) {
+		console.log("creating ColumnSet");
 		if(!ws || !ws['!ref']) return;  // nullcheck
 		var range = X.utils.decode_range(ws['!ref']);
 		// c -> col, r-> row, s-> start cell in range, e-> end cell in range
 		if(!range || !range.s || !range.e || range.s > range.e) return;
+
   		var R = range.s.r; // index of first row
   		var C = range.s.c; // index of first col
 
-  		var names = new Array(range.e.c-range.s.c+1);
+  		var names = []; // column names
   		for(C = range.s.c; C<= range.e.c; ++C) {  // getting column names
 		    var addr = X.utils.encode_cell({c:C,r:R});
-		    names[C-range.s.c] = ws[addr] ? ws[addr].v : X.utils.encode_col(C);
+		    ws[addr] ? names.push(ws[addr].v) : names.push(X.utils.encode_col(C));
 		}
-		names[0] = "ID";  // hardcoding the first column to be id
-		for (let i = 0; i < names.length; i++) { 
-			var mystr = "\"";
-			names[i] = mystr.concat(names[i]).concat(mystr);
+
+		// POSTGRESQL types are defaulted by pg-promise, not currently being set
+		var columnSet = [];
+		// can't have 0 as column name, must catch validation errors
+		for (let i = 0; i < names.length; i++) {
+			let cleanString;
+			if (typeof names[i] === 'string' || names[i] instanceof String) {
+				cleanString = names[i].replace(/[^\w\s]/gi, ' '); // only allowing js var valid chars
+				cleanString = cleanString.trim();  
+				cleanString = cleanString.replace(/ /g,'_'); // removing spaces
+				cleanString = cleanString.replace(/[^\w\s]|(_)\1/gi,'_'); // replacing consecutive '__' with '_'
+				cleanString = cleanString.toLowerCase();
+			}
+			headers.push(cleanString);
+			columnSet.push(new Column({
+				name: cleanString,
+				def: null
+			}));
 		}
-		//console.log(names);
 
-
-	  var types = new Array(range.e.c-range.s.c+1);
-	  for(C = range.s.c; C<= range.e.c; ++C) {
-	    var seen = {}, _type = "";
-	    for(R = range.s.r+1; R<= range.e.r; ++R)
-	      seen[(ws[X.utils.encode_cell({c:C,r:R})]||{t:"z"}).t] = true;
-	    if(seen.s || seen.str) _type = TYPES.t;
-	    else if(seen.n + seen.b + seen.d + seen.e > 1) _type = TYPES.t;
-	    else switch(true) {
-	      case seen.b: _type = TYPES.b; break;
-	      case seen.n: _type = TYPES.n; break;
-	      case seen.e: _type = TYPES.t; break;
-	      case seen.d: _type = TYPES.d; break;
-	    }
-	    types[C-range.s.c] = _type || TYPES.t;
-	  }
-
-	  var out = [];
-
-	  var BT = mode == "PGSQL" ? "" : "`";
-	  var Q  = mode == "PGSQL" ? "'" : '"';
-	  var J  = mode == "PGSQL" ? /'/g : /"/g;
-	  out.push("DROP TABLE IF EXISTS " + BT + sname + BT );
-	  out.push("CREATE TABLE " + BT + sname + BT + " (" + names.map(function(n, i) { 
-	  		return BT + n + BT + " " + (types[i]||"TEXT"); }
-	  	).join(", ") + ", PRIMARY KEY (" + names[0] + ")" + ");"); 
-
-	  for(R = range.s.r+1; R<= range.e.r; ++R) {
-	    var fields = [], values = [];
-	    for(C = range.s.c; C<= range.e.c; ++C) {
-	      var cell = ws[X.utils.encode_cell({c:C,r:R})];
-	      if(!cell) continue;
-	      fields.push(BT + names[C-range.s.c] + BT);
-	      var val = cell.v;
-	      switch(types[C-range.s.c]) {
-	        case TYPES.n: if(cell.t == 'b' || typeof val == 'boolean' ) val = +val; break;
-	        default: val = Q + val.toString().replace(J, Q + Q) + Q;
-	      }
-	      values.push(val);
-	    }
-	    out.push("INSERT INTO " + BT +sname+ BT + " (" + fields.join(", ") + ") VALUES (" + values.join(",") + ");");
-	  }
-
-	  return out;
-
+		return columnSet;
 	}
 };
--- react-backend/routes/upload.js
+++ react-backend/routes/upload.js
@@ -13,41 +13,83 @@ var upload = multer({ storage: storage });
 var dbUtil = require('../db/util.js');
 dbUtil.connect();  // create and connect to pool
 
+
+// new shit testing ----------------------------
+const pgp = require('pg-promise')({
+    capSQL: true, // if you want all generated SQL capitalized
+});
+const connectionString = 'postgres://vuitsnnr:qfNR6FucYV92dloKvm7QOrj7ifx5pk6c@stampy.db.elephantsql.com:5432/vuitsnnr';
+const db = pgp(connectionString);
+// --------------------------------------
+
+var data;
+
+// returns promise that reps next data that will be available
+// array of objects up till pageIndex, unless data doesn't have that many
+function getNextData(smth, index) {
+  return new Promise((resolve) => {
+    let lastIndex = (index + 1) * 10000;
+    let beforeIndex = index * 10000;
+    if (beforeIndex < data.length) {
+        resolve(data.slice(beforeIndex, lastIndex)); // doesn't include lastIndex
+    } else {
+        resolve(null);
+    }
+  });
+}
+
+
+
 // todo: only handling one file upload right now
 router.post('/', upload.any(), function(req, res, next) {
   	console.log('in upload router');
   	var files = req.files;
-  	//console.log(files);
   	var f = files[0];
   	var buffer = f.buffer;
-  	//console.log(buffer);
 
   	dbUtil.getClient((err, client, done) => {
   		if (err) {
   			res.status(500).send("Error retrieving client");
   		} 
-  		
-  		//console.log("Got client successfully");
-  		// reading file
-  		data = new Uint8Array(buffer);
-  		console.log("reading excel file...");
-  		var workBook = XLSX.read(data, {type: 'array'});
 
-  		// NOTE! only works for a sheet named data
-  		var queries = SheetJSSQL.sheetToQueries(workBook.Sheets['Data'], 'Data', "PGSQL");
-      for (let i = 0; i < queries.length; i++) {
-        //console.log(queries[i]);
-        client.query(queries[i])
-          .then(res => console.log("query " + i + " was successful"))
-          .catch(e => {
-            console.error(e.stack);
-          });
-      }
+  	// reading file
+		data = new Uint8Array(buffer);
+		console.log("reading excel file...");
+		var workBook = XLSX.read(data, {type: 'array'});
+    var dataSheet = workBook.Sheets['Data'];
 
-  		//done(); put this in correct spot
-      // make this faster
-      // make this a transaction
-      res.status(201).send("uploaded into db successfully");
+    var columnSetObject = SheetJSSQL.createColumnSet(dataSheet, 'Data', "PGSQL");
+    var headers = SheetJSSQL.getHeaders();  // formatted column headers
+
+    data = XLSX.utils.sheet_to_json(workBook.Sheets['Data'],
+                                            { header: headers,
+                                              range: 1
+                                            });
+    const cs = new pgp.helpers.ColumnSet(columnSetObject);
+    const tablename = new pgp.helpers.TableName('othertable');
+
+    db.tx('massive-insert', t => { // executes callback function as a transaction
+        return t.sequence(index => {  // t.sequence returns a promise
+            return getNextData(t, index)
+                .then(d => {
+                    if (d) {
+                        const insert = pgp.helpers.insert(d, cs, tablename);
+                        return t.none(insert);
+                    }
+                });
+        });
+    }).then(d => {
+        // COMMIT has been executed
+        console.log('Total batches:', d.total, ', Duration:', d.duration);
+        res.status(201).send("uploaded into db successfully");
+    }).catch(error => {
+        // ROLLBACK has been executed
+        console.log(error);
+        res.status(500).send("did not upload successfully");
+    });
+
+    // drop table if have to command  , note... does this data have a primary key?
+    // create table ...
 
   	// cancel file upload halfway? how to stop
 
@@ -56,63 +98,9 @@ router.post('/', upload.any(), function(req, res, next) {
   	// 	check file is excel file
 
   	// 	check if the formatting is correct
+
+    // catch duplicate column names, wrap in try block or something
   	});
 });
-
-// router.post('/', upload.any(), function(req, res, next) {
-//     console.log('in upload router');
-//     var files = req.files;
-
-//     var f = files[0];
-//     var buffer = f.buffer;
-
-//     // reading file
-//     data = new Uint8Array(buffer);
-//     console.log("reading excel file...");
-//     var workBook = XLSX.read(data, {type: 'array'});
-//     var queries = SheetJSSQL.sheetToQueries(workBook.Sheets['Data'], 'Data', "PGSQL"); // NOTE! only works for a sheet named data
-//     var partition = queries.length / 10;
-
-//     for (let i = 0; i < 10; i++) { // our pool only has 10 clients right now
-//       dbUtil.getClient((err, client, done) => {
-//         console.log("aquired client " + i);
-//         if (err) {
-//           res.status(500).send("Error retrieving client");
-//         } 
-//         for (let i = 0; i < queries.length; i++) {
-//           client.query(queries[i])
-//             .then(res => console.log("query " + i + " was successful"))
-//             .catch(e => {
-//               console.error(e.stack);
-//             });
-//         }
-//       });
-//     }
-
-//     dbUtil.getClient((err, client, done) => {
-//       if (err) {
-//         res.status(500).send("Error retrieving client");
-//       } 
-      
-//       for (let i = 0; i < queries.length; i++) {
-//         client.query(queries[i])
-//           .then(res => console.log("query " + i + " was successful"))
-//           .catch(e => {
-//             console.error(e.stack);
-//           });
-//       }
-
-//       //done(); put this in correct spot
-//       //res.status(201).send("uploaded into db successfully");
-
-//     // cancel file upload halfway? how to stop
-
-//     //  use filename as new table name...? find out how to submit the file name
-
-//     //  check file is excel file
-
-//     //  check if the formatting is correct
-//     });
-// });
 
 module.exports = router;
